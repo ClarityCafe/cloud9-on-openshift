93b55333492cdb0a9c42aedaf3e59929dc6ca3c2
Capuccino <enra@sayonika.moe>
2018-10-26 18:40:08 +0800

    Update env vars to match with community fork

--- v2/run_usercommand.sh
+++ v2/run_usercommand.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -z "$C9_PASSWORD" ] && [ ! -z "$C9_USERNAME" ]; then
+if [ ! -z "$PLN_PASSWORD" ] && [ ! -z "$PLN_USERNAME" ]; then
    echo "Starting Cloud9..."
-   node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $C9_USERNAME --password $C9_PASSWORD;
+   node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $PLN_USERNAME --password $PLN_PASSWORD;
 else
-   echo "[warn] Starting Cloud9 Without Authorization is highly discouraged. Set them from the C9_USERNAME and C9_PASSWORD variables!"
+   echo "[warn] Starting Cloud9 Without Authorization is highly discouraged. Set them from the PLN_USERNAME and PLN_PASSWORD variables!"
    node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181;
 fi
