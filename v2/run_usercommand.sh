763145dd40fe062c7ea2516e0bedbe9fc7593ada
Capuccino <enra@sayonika.moe>
2018-11-06 05:55:37 +0000

    optimize Node runtimes for lowmems

--- v2/run_usercommand.sh
+++ v2/run_usercommand.sh
@@ -2,8 +2,8 @@
 
 if [ ! -z "$PLN_PASSWORD" ] && [ ! -z "$PLN_USERNAME" ]; then
    echo "Starting Cloud9..."
-   node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $PLN_USERNAME --password $PLN_PASSWORD;
+   $(command -v node) --expose-gc --always-compact --max-old-space-size=128 cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $PLN_USERNAME --password $PLN_PASSWORD;
 else
    echo "[warn] Starting Cloud9 Without Authorization is highly discouraged. Set them from the PLN_USERNAME and PLN_PASSWORD variables!"
-   node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181;
+   $(command -v node) --expose-gc --always-compact --max-old-space-size=128 cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181;
 fi
